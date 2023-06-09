97b2fc080074224d8f5ba614b1edca3aba3164f2
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-06-09 12:30:54 -0400

    A class was created for the snake.

---
+++ NewSnakeProj/Snake.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace NewSnakeProj
+{
+    internal class Snake // Clase Serpiente
+    {
+        public bool Vivo { get; set; } // vida de la serpiente.
+        public ConsoleColor HeadColor { get; set; } // color de cabeza de la serpiente.
+        public ConsoleColor BodyColor { get; set; } // color de cuerpo de la serpiente.
+        public Ventana Window { get; set; } // Ventana de la consola.
+        public List<Point> Body { get; set; } // cuerpo de la serpiente.
+        public Point Head { get; set; } // Cabeza de la serpiente.
+
+        // Crearemos un constructor con sus respectivos parámetros para la clase Snake
+
+        public Snake(Point posicion, ConsoleColor colorCabeza, ConsoleColor colorCuerpo,
+            Ventana ventana)
+        {
+            HeadColor = colorCabeza; // Color de cabeza.
+            BodyColor = colorCuerpo; // Color de cuerpo.
+            Window = ventana; // Ventana de la consola.
+            Head = posicion; // Posición de la cabeza.
+            Body = new List<Point>(); // Cuerpo definido según crecimiento de la serpiente.
+        }
+    }
+}
