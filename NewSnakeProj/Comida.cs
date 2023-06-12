46fbf4c0518b9be1debf91a8f3b5cc00fac01579
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-06-12 02:38:24 -0400

    Added a test food for the snake.

---
+++ NewSnakeProj/Comida.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace NewSnakeProj
+{
+    // Crearemos nuestra clase para la comida.
+    internal class Comida
+    {
+        // Definiremos las variables para la comida de la serpiente.
+
+        public Point Posicion { get; set; } // Posicion de la comida.
+        public ConsoleColor Color { get; set; } // Color para la comida.
+        public Ventana Window { get; set; } // Ventana de la consola para la comida.
+
+        // Crearemos un constructor para la clase Comida sin contar con la posición en donde se encuentra.
+
+        public Comida(ConsoleColor color, Ventana ventana)
+        {
+            Color = color; // Color
+            Window = ventana; // Ventana de la consola.
+        }
+
+        // Crearemos un nuevo método que permita visualizar la comida dependiendo de la posición en donde se encuentra.
+
+        private void Dibujar()
+        {
+            Console.ForegroundColor = Color;
+            Console.SetCursorPosition(Posicion.X, Posicion.Y);
+            Console.Write("█"); // ALT 219
+        }
+
+        // Crearemos un nuevo método que permita generar una comida para la serpiente y llamaremos al método anterior para su posterior ejecución.
+
+        public void GenerarComida()
+        {
+            Random random = new Random(); // Crearemos una variable de tipo Random.
+            int x = random.Next(Window.LimiteSuperior.X + 1, Window.LimiteInferior.X); // Para la coordenada en X.
+            int y = random.Next(Window.LimiteSuperior.Y + 1, Window.LimiteInferior.Y); // Para la coordenada en Y.
+            Posicion = new Point(x, y); // Ubicaremos la posición dependiendo de la coordenada en el punto x,y.
+            Dibujar(); // Llamado del método anterior.
+        }
+
+    }
+}
--- NewSnakeProj/Program.cs
+++ NewSnakeProj/Program.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 
 Ventana ventana;
 Snake snake;
+Comida comida;
 bool jugar = true;
 
 void Iniciar()
@@ -10,8 +11,11 @@ void Iniciar()
     ventana = new Ventana("Snake", 65, 20, ConsoleColor.DarkYellow, ConsoleColor.DarkGreen,
     new Point(5, 3), new Point(59, 18));
     ventana.DibujarMarco();
+    comida = new Comida(ConsoleColor.DarkGray, ventana);
     snake = new Snake(new Point(8, 5), ConsoleColor.DarkRed, ConsoleColor.Red, ventana);
     snake.IniciarCuerpo(2);
+
+    comida.GenerarComida(); // Con ese método siempre se debe dejar público.
 }
 
 void Game()
