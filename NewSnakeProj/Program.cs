794650baa289bcb4ac40e22bc1691d9cb3d49b9d
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-06-16 13:30:26 -0400

    The whole process of our game is finished by modifying all its features and adding the main menu including the additional snake and modifying some settings to the scores.

--- NewSnakeProj/Program.cs
+++ NewSnakeProj/Program.cs
@@ -5,7 +5,7 @@ Ventana ventana; // Variable ventana.
 Snake snake; // Variable para la serpiente.
 Comida comida; // Variable para la comida.
 bool ejecutar = true; // Ejecución si responde o no.
-bool jugar = false; // Por el momento mostraremos el menú principal del juego.
+bool jugar = false; // Se realizaron todos los ajustes correctamente a la interfaz del juego.
 
 // Método para inicializar el juego.
 void Iniciar()
@@ -24,17 +24,25 @@ void Game()
     while (ejecutar) // Para ejecutar el juego completo.
     {
         ventana.Maingame();
-        ventana.Teclado(ref ejecutar, ref jugar);
+        ventana.Teclado(ref ejecutar, ref jugar, snake);
         while (jugar) // Mientras se ejecuta el juego.
         {
             snake.Informacion(0, 34); // Información de la serpiente.
             snake.Mover(); // Movimiento de la serpiente.
-            Thread.Sleep(50); // Duerme el programa durante 50 milisegundos mediante hilos de ejecución.
+
+            if (!snake.Vivo) // Verificaremos si la serpiente está muerta.
+            {
+                jugar = false; // No va a seguir jugando.
+                snake.Puntaje = 0; // Se inicializa el puntaje en 0.
+            }
+
+            Thread.Sleep(100); // Duerme el programa durante 50 milisegundos mediante hilos de ejecución.
         }
+        Thread.Sleep(100); // Para que la serpiente del menú no se mueva muy rápido.
     }
 
 }
 
 Iniciar(); // Llamado del método al iniciar el programa.
 Game(); // Llamado del método al juego.
-Console.ReadKey(); // Se muestra la lectura de la consola al terminar su ejecución.
+
